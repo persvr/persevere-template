ca968a315719ad0097c8a2ace2ad049f99aee34d
unknown <Kris@.(none)>
2010-05-12 22:35:49 -0600

    move from pintura

---
+++ jackconfig.js
@@ -0,0 +1,51 @@
+/**
+ * The starting point for Pintura running as a Jack app.
+ */
+try{
+	var pintura = require("pintura/pintura");
+}catch(e){
+	// old loaders need to use this type of access, pintura will fix things from there 
+	require("./lib/util/narwhal-compat");
+	pintura = require("pintura/pintura");
+}
+
+var File = require("file"),
+	transporter = require("pintura/jsgi/transporter");
+
+require("app");
+
+// setup the Jack application
+exports.app =
+	// this will provide module wrapping for the server side CommonJS libraries for the client
+	transporter.Transporter({loader: function(id){
+		if(id.match(/-engine/)){
+			id = "../engines/default/lib/" + id;
+		}
+		return require.loader.loader.fetch(require.loader.resolvePkg(id.substring(0, id.length - 3),"","","")[0]);
+	}}, 
+ 	// main Pintura handler 
+ 	pintura.app
+	);
+
+
+var perseverePath = require("packages").resource("public/explorer.html").toString();
+perseverePath = perseverePath.substring(0, perseverePath.length - "/explorer.html".length).replace(/\\/,'/');
+// now setup the development environment, handle static files before reloading the app
+// for better performance
+exports.development = function(app, options){
+	return require("jack/cascade").Cascade([
+			// cascade from static to pintura REST handling
+
+		// the main place for static files accessible from the web
+		require("jack/static").Static(null, {urls:[""],root:"public"}),
+		require("jack/static").Static(null, {urls:["/explorer"],root:perseverePath}),
+		require("jack/static").Static(null, {urls:["/js/dojo-persevere"],root:perseverePath}),
+		// the typical reloader scenario
+		(!options || options.reload) ? require("jack/reloader").Reloader(File.join(File.cwd(), "jackconfig"), "app") :
+								exports.app
+	]);
+};
+
+// we start the REPL (the interactive JS console) because it is really helpful
+new (require("worker").SharedWorker)("narwhal/repl");
+
