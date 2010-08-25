7781650722598011d93609ef0f3fb8b8b2d73b7b
Kris Zyp <kriszyp@gmail.com>
2010-08-25 13:33:38 -0600

    Switch to using nodules for module loading

--- jackconfig.js
+++ jackconfig.js
@@ -1,43 +1,32 @@
 /**
  * The starting point for Pintura running as a Jack app.
  */
-try{
-	var pintura = require("pintura/pintura");
-}catch(e){
-	// old loaders need to use this type of access, pintura will fix things from there 
-	require("./lib/util/narwhal-compat");
-	pintura = require("pintura/pintura");
-}
+var pinturaApp;
+require("nodules").useLocal().ensure(["pintura/pintura", "app", "tunguska/jack-connector"], function(require){
+	require.reloadable(function(){
+		pinturaApp = require("pintura/pintura").app;
+		require("app");
+	});
+	require("tunguska/jack-connector").observe("worker", pinturaApp.addConnection);
+	// we start the REPL (the interactive JS console) because it is really helpful
+	new (require("worker").SharedWorker)("narwhal/repl");
+});
 
 var File = require("file"),
-	transporter = require("transporter/jsgi/transporter");
-
-require("app");
-// setup the Jack application
-exports.app =
-	// this will provide module wrapping for the server side CommonJS libraries for the client
-	transporter.Transporter({loader: function(id){
-		if(id.match(/promised-io/)){
-			id = id.replace(/promised-io\/http-client/,'http-client');
-		}
-		return require.loader.loader.fetch(require.loader.resolvePkg(id.substring(0, id.length - 3),"","","")[0]);
-	}}, 
-	 	// main Pintura handler 
-		pintura.app
-	);
-
+	Transporter = require("jsgi/transporter").Transporter;
 
 var perseverePath, 
 	Static = require("jack/static").Static,
 	Directory = require("jack/dir").Directory;
-
+	
 var path = require.paths[0].match(/(.*?)[\/\\]packages[\/\\]/);
 if(path){
 	perseverePath = path[1] + "/packages/persevere/public";
 }
+
 // now setup the development environment, handle static files before reloading the app
 // for better performance
-exports.development = function(app, options){
+exports.app = exports.development = function(app, options){
 	return require("jack/cascade").Cascade([
 			// cascade from static to pintura REST handling
 /*		// this will provide module wrapping for the Dojo modules for the client
@@ -49,14 +38,12 @@ exports.development = function(app, options){
 		// the main place for static files accessible from the web
 		Directory("public", Static(null, {urls:[""], root: "public"})),
 		Static(null, {urls:["/explorer"], root: perseverePath + "/explorer"}),
-		// the typical reloader scenario
-		(!options || options.reload) ? require("jack/jack/reloader").Reloader(File.join(File.cwd(), "jackconfig"), "app") :
-								exports.app
-	]);
+		// this will provide access to the server side JS libraries from the client
+		Transporter({loader: require("nodules").forEngine("browser").useLocal().getModuleSource}),
+		
+		 	// main Pintura handler 
+			function(request){
+				return pinturaApp(request);
+			}
+		]);
 };
-
-require("tunguska/jack-connector").observe("worker", pintura.app.addConnection);
-
-// we start the REPL (the interactive JS console) because it is really helpful
-new (require("worker").SharedWorker)("narwhal/repl");
-
