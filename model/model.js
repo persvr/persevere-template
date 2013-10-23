9e473db46c0fac174658541c9ac634ad8f540381
Kris Zyp <kriszyp@gmail.com>
2013-10-23 07:08:05 -0600

    Add model template

---
+++ model/model.js
@@ -0,0 +1,8 @@
+var Model = require("perstore/model").Model,
+	DefaultStore = require("perstore/stores").DefaultStore,
+	store = DefaultStore();
+
+// define our model
+module.exports = Model(store, {
+	
+});
